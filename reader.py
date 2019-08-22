95a696a12776642f780c671eea027f0b6c5ca7b3
Oisín Smith <oisin@oisinsmith.eu>
2019-08-22 18:40:09 +0200

    Select thing

--- reader.py
+++ reader.py
@@ -21,43 +21,18 @@ with open("data.igc", "r") as f:
     parsed_igc_file = igc_reader.read(f)
 
 class Rectangle:
-    def __init__(self, start_x, press, ax):
-        self.rect = patches.Rectangle((50,100),40,30,linewidth=1,edgecolor='r',facecolor='none')
-        ax.add_patch(self.rect)
-        self.press = press
+    def __init__(self):
+        self.x0 = 0
+        self.x1 = 0
+        self.mousex = 0
 
-    def connect(self):
-        'connect to all the events we need'
-        # self.cidpress = self.rect.figure.canvas.mpl_connect(
-        #     'button_press_event', self.on_press)
-        # self.cidrelease = self.rect.figure.canvas.mpl_connect(
-        #     'button_release_event', self.on_release)
-        
-
-    def on_press(self, event):
-        'on button press we will see if the mouse is over us and store some data'
-        if event.inaxes != self.rect.axes: return
-
-        contains, attrd = self.rect.contains(event)
-        if not contains: return
-        print('event contains', self.rect.xy)
-        x0, y0 = self.rect.xy
-        self.press = x0, y0, event.xdata, event.ydata
+    def start(self, event):
+        self.x0 = event.xdata
 
     def on_motion(self, event):
         'on motion we will move the rect if the mouse is over us'
-        if self.press is None: return
-        if event.inaxes != self.rect.axes: return
-        x0, y0, xpress, ypress = self.press
-        dx = event.xdata - xpress
-        dy = event.ydata - ypress
-        #print('x0=%f, xpress=%f, event.xdata=%f, dx=%f, x0+dx=%f' %
-        #      (x0, xpress, event.xdata, dx, x0+dx))
-        self.rect.set_x(x0+dx)
-        self.rect.set_y(y0+dy)
-
-        self.rect.figure.canvas.draw()
-
+        if event.xdata:
+            self.mousex = event.xdata
 
     def on_release(self, event):
         'on release we reset the press data'
@@ -71,30 +46,37 @@ class Rectangle:
         self.rect.figure.canvas.mpl_disconnect(self.cidmotion)
 
     def draw(self, ax):
-    	ax.bar()
+        ax.bar(self.x0, 500, self.mousex - self.x0, color=(1, 0, 0, 0.5), align='edge')
+
+    def finish(self):
+        pass
 
 def normalise_datetime(df):
-	to_datetime = partial(datetime.datetime.combine, date)
-	df["time"] = df["time"].apply(to_datetime)
-	df.set_index("time", inplace=True)
-	return df
+    to_datetime = partial(datetime.datetime.combine, date)
+    df["time"] = df["time"].apply(to_datetime)
+    df.set_index("time", inplace=True)
+    return df
 
 def resample(df, freq=1):
     return df.resample("1s", base=0).mean().dropna()
 
 def onclick(event):
-	if editing_rect:
-		editing_rect.finish()
-		editing_rect = None
-	else:
-		rect = Rectangle()
-		rect.start(event.xdata, event, ax1)
-		rects.append(rect)
-		editing_rect = rect
+    global editing_rect
+
+    if editing_rect:
+        editing_rect.finish()
+        editing_rect = None
+    else:
+        rect = Rectangle()
+        rect.start(event)
+        rects.append(rect)
+        editing_rect = rect
 
 def onmotion(event):
-	if editing_rect:
-		editing_rect.on_motion(event)
+    global editing_rect
+
+    if editing_rect:
+        editing_rect.on_motion(event)
 
 m = 818  # kg
 g = 9.81  # m/s^2
@@ -114,18 +96,18 @@ df["alt_rol"] = df["alt"].rolling(5).mean()
 ax1.grid()
 
 def draw(_):
-	global ax1
+    global ax1
 
-	ax1.clear()
-	ax2.clear()
+    ax1.clear()
+    ax2.clear()
 
-	df[["alt", "alt_rol"]].plot(ax=ax1)
-	df[["pressure_alt"]].plot(ax=ax2, color="green", linewidth=1)
+    df[["alt", "alt_rol"]].plot(ax=ax1)
+    df[["pressure_alt"]].plot(ax=ax2, color="green", linewidth=1)
 
-	for rect in rects:
-		rect.draw(ax1)
+    for rect in rects:
+        rect.draw(ax1)
 
-ani = animation.FuncAnimation(fig, draw, interval=500)
+ani = animation.FuncAnimation(fig, draw, interval=20)
 
 fig.canvas.mpl_connect('button_press_event', onclick)
 fig.canvas.mpl_connect('motion_notify_event', onmotion)
