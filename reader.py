8335f33d0d0ea3bf2476df79c13e4b2a18a4331f
Oisín Smith <oisin@oisinsmith.eu>
2019-08-23 00:47:34 +0200

    selection working

--- reader.py
+++ reader.py
@@ -15,7 +15,7 @@ igc_reader = Reader()
 
 date = date(2019, 8, 21)
 rects = []
-fig, (ax_data_1, ax_zones) = plt.subplots(2, 1)
+fig, (ax_data_1, ax_zones) = plt.subplots(2, 1, sharex=False)
 # fig, ax_data_1 = plt.subplots()
 ax_data_2 = ax_data_1.twinx()
 
@@ -32,7 +32,7 @@ def normalise_datetime(df):
     return df
 
 def resample(df, freq=1):
-    return df.resample("1s", base=0).mean().dropna()
+    return df.resample("1s", base=0).interpolate(method="time").dropna()
 
 def onselect(vmin, vmax):
     global df
@@ -45,21 +45,17 @@ def onselect(vmin, vmax):
     ax_data_2.axvspan(vmin, vmax, alpha=0.5)
     print(vmin, vmax)
 
-    df = df.dropna()
-
-    vmin_dt = mdate.num2date(vmin).replace(tzinfo=None)
-    vmax_dt = mdate.num2date(vmax).replace(tzinfo=None)
+    vmin_dt = datetime.fromtimestamp(vmin)
+    vmax_dt = datetime.fromtimestamp(vmax)
 
     selections = df.index.to_series()
     selections = pd.to_numeric(selections)
     selections[:] = np.nan
     # return
-    print(mdate.num2date(vmin))
-    print(selections.index[0])
-    print(selections[vmin_dt:vmax_dt])
-    selections.loc[vmin_dt:vmax_dt] = df["alt_rol"][vmin_dt:vmax_dt].mean()
+    selections.loc[vmin_dt:vmax_dt] = df["vs"][vmin_dt:vmax_dt].mean()
 
     print(selections)
+    print(selections[selections.notna().any()])
 
     #ax_zones.clear()
     ax_zones.plot(selections)
@@ -70,25 +66,32 @@ g = 9.81  # m/s^2
 print(f"Parsing logger file from {parsed_igc_file['header'][1]['glider_registration']}")
 
 df = pd.DataFrame(parsed_igc_file["fix_records"][1])
+df = df.drop(["validity"], axis=1)
 df = normalise_datetime(df)
 df = resample(df)
 df["TAS"] = df["TAS"] / 100 / 3.6  # m/s
 df["Ekin"] = 0.5 * m * df["TAS"] ** 2  # J
 df["Epot"] = m * df["gps_alt"] * g  # J
 df["E"] = df["Ekin"] + df["Epot"]  # J
-df["alt"] = np.gradient(df["pressure_alt"], edge_order=2)
-df["alt_rol"] = df["alt"].rolling(5).mean()
 
-ax_data_1.grid()
+# print(df.diff()["pressure_alt"])
+# print(df.index.to_series().diff())
+# print(df.diff()["pressure_alt"] / df.index.to_series().diff())
+# df["alt"] = np.gradient(df["pressure_alt"])
+df["vs"] = df["pressure_alt"].diff()
+df["vs_smooth"] = df["vs"].rolling(5).mean()
+
+ax_data_2.grid()
 
 # ani = animation.FuncAnimation(fig, draw, interval=20)
 
 rectprops = dict(facecolor='blue', alpha=0.5)
 
-df[["alt", "alt_rol"]].plot(ax=ax_data_1)
+df[["vs", "vs_smooth"]].plot(ax=ax_data_1)
 df[["pressure_alt"]].plot(ax=ax_data_2, color="green", linewidth=1)
 
 span = mwidgets.SpanSelector(ax_data_2, onselect, 'horizontal', span_stays=True, rectprops=rectprops)
+print(df[df.isna().any(axis=1)].head(50))
 
 plt.show()
 # df.to_csv("data.csv")
